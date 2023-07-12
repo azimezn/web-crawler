429cadb842c4ff04f30def2c8e8176158dd20129
azimezn <117550865+azimezn@users.noreply.github.com>
2023-07-12 14:04:12 -0400

    wrapped it in a try catch block. can extract data from one page.

--- server.js
+++ server.js
@@ -6,37 +6,43 @@ const cheerio = require("cheerio");
 async function crawl() {
     console.log("entered crawl function");
 
-    // HTTP GET request to store the HTML content, which is the response, on given webpage
-    // await waits for the response to be received before moving on
-    const pageHTML = await axios.get("https://www.georgiamls.com/real-estate-agents/directory/a/1");
+    // wrapping the code in a try block to catch and handle potential errors
+    try {
 
-    // makes it possible to manipulate the HTML from the webpage
-    const $ = cheerio.load(pageHTML.data);
+        // HTTP GET request to store the HTML content, which is the response, on given webpage
+        // await waits for the response to be received before moving on
+        const pageHTML = await axios.get("https://www.georgiamls.com/real-estate-agents/directory/a/1");
 
-    // // goes through each letter page
-    // // !!!!!!!! this goes through first page of each letter. i need to go through every page of each letter
-    // $("#alphabet li a").each((index, element) => {
-    //     const paginationURL = $(element).attr("href")
-    // })
+        // makes it possible to manipulate the HTML from the webpage
+        const $ = cheerio.load(pageHTML.data);
 
-    // an array to store all data of agents
-    const agents = [];
+        // // goes through each letter page
+        // // !!!!!!!! this goes through first page of each letter. i need to go through every page of each letter
+        // $("#alphabet li a").each((index, element) => {
+        //     const paginationURL = $(element).attr("href")
+        // })
 
-    // retrieving the data through each row
-    $("tr").each((index, element) => {
-        const agentName = $(element).find("td:nth-child(1) a").text().trim();
-        const phone = $(element).find("td:nth-child(2) a").text().trim();
-        const phoneOffice = $(element).find("td:nth-child(3) a").text().trim();
-        const office = $(element).find("td:nth-child(4) a").text().trim();
-  
-        // create an object with the information
-        const agent = { agentName, phone, phoneOffice, office };
-        // add the object to the agents array
-        agents.push(agent);
-      });
+        // an array to store all data of agents
+        const agents = [];
 
+        // retrieving the data through each row
+        $("tr").each((index, element) => {
+            const agentName = $(element).find("td:nth-child(1) a").text().trim();
+            const phone = $(element).find("td:nth-child(2) a").text().trim();
+            const phoneOffice = $(element).find("td:nth-child(3) a").text().trim();
+            const office = $(element).find("td:nth-child(4) a").text().trim();
 
-      
+            // create an object with the information
+            const agent = { agentName, phone, phoneOffice, office };
+            // add the object to the agents array
+            agents.push(agent);
+        });
+
+        // show the agent data
+        console.log("Agents:", agents);
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
 
 };
 
