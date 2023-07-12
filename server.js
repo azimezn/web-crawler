1bbea177231048b52df43889914af9f7fc7a6200
azimezn <117550865+azimezn@users.noreply.github.com>
2023-07-12 14:44:12 -0400

    successfully gets data from the first page of each letter

--- server.js
+++ server.js
@@ -9,34 +9,45 @@ async function crawl() {
     // wrapping the code in a try block to catch and handle potential errors
     try {
 
-        // HTTP GET request to store the HTML content, which is the response, on given webpage
-        // await waits for the response to be received before moving on
-        const pageHTML = await axios.get("https://www.georgiamls.com/real-estate-agents/directory/a/1");
-
-        // makes it possible to manipulate the HTML from the webpage
-        const $ = cheerio.load(pageHTML.data);
-
-        // // goes through each letter page
-        // // !!!!!!!! this goes through first page of each letter. i need to go through every page of each letter
-        // $("#alphabet li a").each((index, element) => {
-        //     const paginationURL = $(element).attr("href")
-        // })
+        const baseURL = "https://www.georgiamls.com/";
 
         // an array to store all data of agents
         const agents = [];
 
-        // retrieving the data through each row
-        $("tr").each((index, element) => {
-            const agentName = $(element).find("td:nth-child(1) a").text().trim();
-            const phone = $(element).find("td:nth-child(2) a").text().trim();
-            const phoneOffice = $(element).find("td:nth-child(3) a").text().trim();
-            const office = $(element).find("td:nth-child(4) a").text().trim();
+        // to iterate over the alphabet letters
+        const letters = "abcdefghijklmnopqrstuvwxyz".split("");
 
-            // create an object with the information
-            const agent = { agentName, phone, phoneOffice, office };
-            // add the object to the agents array
-            agents.push(agent);
-        });
+        // loop over each letter
+        for (const letter of letters) {
+
+            let currentPageURL = `${baseURL}/real-estate-agents/directory/${letter}/1`
+
+            while (currentPageURL) {
+                console.log("checking page:", currentPageURL);
+
+                // HTTP GET request to store the HTML content, which is the response, on given webpage
+                // await waits for the response to be received before moving on
+                const currentPageHTML = await axios.get(currentPageURL);
+                // makes it possible to manipulate the HTML from the webpage
+                const $ = cheerio.load(currentPageHTML.data);
+
+                // retrieving the data through each row of current page
+                $("tr").each((index, element) => {
+                    const agentName = $(element).find("td:nth-child(1) a").text().trim();
+                    const phone = $(element).find("td:nth-child(2) a").text().trim();
+                    const phoneOffice = $(element).find("td:nth-child(3) a").text().trim();
+                    const office = $(element).find("td:nth-child(4) a").text().trim();
+
+                    // create an object with the information
+                    const agent = { agentName, phone, phoneOffice, office };
+                    // add the object to the agents array
+                    agents.push(agent);
+                })
+
+                const nextButton = $(".pagination a.btn-success:contains('Next')");
+                currentPageURL = nextButton.length ? `${baseURL}${nextButton.attr("href")}` : null;
+            }
+        };
 
         // show the agent data
         console.log("Agents:", agents);
