1e33a0e619b42cb0ab4bcc4b813ae832dcb2ec90
azimezn <117550865+azimezn@users.noreply.github.com>
2023-07-16 11:52:02 -0400

    can successfully go through each letter of each page, enter each agent's page and get the information. georgiamls completed.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // used to make HTTP requests
 const axios = require("axios");
+// using axiosRetry because it crashes before finishing
+const axiosRetry = require("axios-retry");
 // used to manipulate HTML
 const cheerio = require("cheerio");
 // used to export data to excel
 const xlsx = require("xlsx");
+
+// axios retires up to 3 times
+axiosRetry(axios, { retries: 3 });
 
 async function crawl() {
     console.log("entered crawl function");
@@ -38,78 +43,82 @@ async function crawl() {
 
             for (let currentPageNumber = 1; currentPageNumber <= totalPages; currentPageNumber++) {
 
-                // let currentPageURL = `${baseURL}/real-estate-agents/directory/${letter}/${currentPageNumber}`
-                let currentPageURL = `${baseURL}/real-estate-agents/directory/a/1`
+                const currentPageURL = `${baseURL}/real-estate-agents/directory/${letter}/${currentPageNumber}`
                 console.log("checking page:", currentPageURL);
 
-                const currentPageHTML = await axios.get(currentPageURL);
-                const $ = cheerio.load(currentPageHTML.data);
+                try {
+                    const currentPageHTML = await axios.get(currentPageURL);
+                    const $ = cheerio.load(currentPageHTML.data);
 
-                // retrieving the data through each row of current page
-                $("tr").each(async (index, element) => {
-                    // get URL for specific agent
-                    const agentURL = $(element).find("td:nth-child(1) a").attr("href");
-                    console.log("checking agent:", agentURL)
+                    // retrieving the data through each row of current page
+                    $("tr").each(async (index, element) => {
+                        // get URL for specific agent
+                        const agentURL = $(element).find("td:nth-child(1) a").attr("href");
+                        console.log("checking agent:", agentURL)
 
-                    // if agentURL is undefined, skip it
-                    if (!agentURL) {
-                        console.log("skipping agent:", agentURL);
-                        return;
-                    }
-                    console.log("agent page html", `${baseURL}${agentURL}`)
+                        // if agentURL is undefined, skip it
+                        if (!agentURL) {
+                            console.log("skipping agent:", agentURL);
+                            return;
+                        }
+                        console.log("agent page html", `${baseURL}${agentURL}`)
 
-                    const agentPageHTML = await axios.get(`${baseURL}${agentURL}`);
-                    const $agentPage = cheerio.load(agentPageHTML.data);
+                        try {
 
-                    // get the elements with the class "col-md-3"
-                    const colMd3Elements = $agentPage('.col-md-3');
+                            const agentPageHTML = await axios.get(`${baseURL}${agentURL}`);
+                            const $agentPage = cheerio.load(agentPageHTML.data);
 
-                    // extract information from the 1st "col-md-3" element
-                    const colMd3 = colMd3Elements.eq(0); // Use .eq(0) to select the first element
+                            // get the elements with the class "col-md-3"
+                            const colMd3Elements = $agentPage('.col-md-3');
 
-                    const agentName = $(element).find("td:nth-child(1) a").text().trim();
-                    const directPhone = colMd3.find('p:nth-child(3) a').text().trim();
-                    const officePhone = colMd3.find('p:nth-child(4) a').text().trim();
-                    const agentWebsite = colMd3.find('p:nth-child(5) a').attr('href');
-                    // const officeName = $(element).find("td:nth-child(4) a").text().trim();
+                            // extract information from the 1st "col-md-3" element
+                            const colMd3 = colMd3Elements.eq(0); // Use .eq(0) to select the first element
 
-                    // its from the 2nd "col-md-3" element
-                    const officeAddressLines = colMd3Elements.eq(1).find('p:nth-child(1)').text().trim();
-                    // format the address
-                    // split at every new line ('\n')
-                    // map through each line and trim
-                    const officeAddress = officeAddressLines.split('\n').map(line => line.trim());
-                    // destructure to get different lines as different variables
-                    const [officeName, addressLine1, addressLine2, phoneNumber] = officeAddress;
-                    // split the addressLine2 and extract the last element, which is always the zip code
-                    const zipCode = addressLine2.split(' ').pop();
+                            const agentName = $(element).find("td:nth-child(1) a").text().trim();
+                            const directPhone = colMd3.find('p:nth-child(3) a').text().trim();
+                            const officePhone = colMd3.find('p:nth-child(4) a').text().trim();
+                            const agentWebsite = colMd3.find('p:nth-child(5) a').attr('href');
+                            // const officeName = $(element).find("td:nth-child(4) a").text().trim();
 
-                    const agent = { agentName, directPhone, officePhone, agentWebsite, officeName, addressLine1, addressLine2, zipCode };
-                    agents.push(agent);
-                    console.log("Agents:", agents);
-                });
+                            // its from the 2nd "col-md-3" element
+                            const officeAddressLines = colMd3Elements.eq(1).find('p:nth-child(1)').text().trim();
+                            // format the address
+                            // split at every new line ('\n')
+                            // map through each line and trim
+                            const officeAddress = officeAddressLines.split('\n').map(line => line.trim());
+                            // destructure to get different lines as different variables
+                            const [officeName, addressLine1, addressLine2, phoneNumber] = officeAddress;
+                            // split the addressLine2 and extract the last element, which is always the zip code
+                            // if no addressLine2, keep it empty
+                            const zipCode = addressLine2 ? addressLine2.split(' ').pop() : '';
 
-
-
-
+                            const agent = { agentName, directPhone, officePhone, agentWebsite, officeName, addressLine1, addressLine2, zipCode };
+                            agents.push(agent);
+                        } catch (error) {
+                            console.error("An error occurred while fetching agent page:", error);
+                        }
+                    });
+                } catch (error) {
+                    console.error("An error occurred while fetching current page:", error);
+                }
             }
         };
 
         // show the agent data - agents is an array of objects
-        // console.log("Agents:", agents);
-        // console.table(agents);
+        console.log("Agents:", agents);
+        console.table(agents);
 
-        // // export the agent data to Excel
-        // const agentDataSheet = xlsx.utils.json_to_sheet(agents);
-        // const agentDataWorkbook = xlsx.utils.book_new();
-        // xlsx.utils.book_append_sheet(agentDataWorkbook, agentDataSheet, "Agent Data");
-        // xlsx.writeFile(agentDataWorkbook, "agent_data.xlsx", { bookType: "xlsx", type: "buffer" }, (err) => {
-        //     if (err) {
-        //         console.error("An error occurred while exporting the agent data:", err);
-        //     } else {
-        //         console.log("Agent data exported to agent_data.xlsx");
-        //     }
-        // });
+        // export the agent data to Excel
+        const agentDataSheet = xlsx.utils.json_to_sheet(agents);
+        const agentDataWorkbook = xlsx.utils.book_new();
+        xlsx.utils.book_append_sheet(agentDataWorkbook, agentDataSheet, "Agent Data");
+        xlsx.writeFile(agentDataWorkbook, "agent_data.xlsx", { bookType: "xlsx", type: "buffer" }, (err) => {
+            if (err) {
+                console.error("An error occurred while exporting the agent data:", err);
+            } else {
+                console.log("Agent data exported to agent_data.xlsx");
+            }
+        });
 
     } catch (error) {
         console.error("An error occurred:", error);
